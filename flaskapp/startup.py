34ac598a711a81bfae12669f74192be0cb627b0a
Drew Michelini <dbmich@umich.edu>
2020-07-27 14:31:28 -0400

    another minor change

--- flaskapp/startup.py
+++ flaskapp/startup.py
@@ -8,21 +8,21 @@ CLIENT_SECRET = "2b6aaa9aa91f4355b5879257301dab48"
 
 #Port and callback url can be changed or ledt to localhost:5000
 PORT = "80"
-CALLBACK_URL = "http://www.drewmi.ch/redirect"
+CALLBACK_URL = "http://www.drewmi.ch"
 
 #Add needed scope from spotify user
 SCOPE = "playlist-modify-private"
-#token_data will hold authentication header with access code, the allowed scopes, and the refresh countdown 
+#token_data will hold authentication header with access code, the allowed scopes, and the refresh countdown
 TOKEN_DATA = []
 
 
 def getUser():
-    return getAuth(CLIENT_ID, "{}:{}/callback/".format(CALLBACK_URL, PORT), SCOPE)
+    return getAuth(CLIENT_ID, "{}:{}/redirect".format(CALLBACK_URL, PORT), SCOPE)
 
 def getUserToken(code):
     global TOKEN_DATA
-    TOKEN_DATA = getToken(code, CLIENT_ID, CLIENT_SECRET, "{}:{}/callback/".format(CALLBACK_URL, PORT))
- 
+    TOKEN_DATA = getToken(code, CLIENT_ID, CLIENT_SECRET, "{}:{}/redirect".format(CALLBACK_URL, PORT))
+
 def refreshToken(time):
     time.sleep(time)
     TOKEN_DATA = refreshAuth()
